d41448d8fcdda099249f3c366b297758b1f1ee58
Jora13 <yyp131@gmail.com>
2021-01-11 23:41:53 +0300

    Learn1

---
+++ proj/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Learn1 from './components/Learn1/Learn1';
+
+function App() {
+  return (
+    <Learn1 />
+  );
+}
+
+export default App;
